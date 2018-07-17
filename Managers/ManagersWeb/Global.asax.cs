dc61de5de5d8979824cb61068ceb63704e03c230
IgorGrodno <37707678+IgorGrodno@users.noreply.github.com>
2018-07-17 16:15:12 +0300

    !!

--- Managers/ManagersWeb/Global.asax.cs
+++ Managers/ManagersWeb/Global.asax.cs
@@ -14,7 +14,7 @@ namespace ManagersWeb
     {
         protected void Application_Start()
         {
-            //Database.SetInitializer<ApplicationDbContext>(new AppDbInitializer());
+           // Database.SetInitializer<ApplicationDbContext>(new AppDbInitializer());
 
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
